dc0203fa2a1fb29035fdbf03ecc00d13fb33e0d7
Raccoon <a60814billy@gmail.com>
2012-09-30 19:56:05 +0800

    fix bug

--- app/Controller/sent.php
+++ app/Controller/sent.php
@@ -1,8 +1,15 @@
 <?php
 class sentController extends OS_Controller{
     
+    public function __construct(){
+        parent::__construct();
+        if($_SESSION['user']['admin']==0){
+            header("Location:".conver_url('./controller=book'));
+        }
+    }
+
     public function index(){
-        
+        $this->_model->init();
     }
 
 }
